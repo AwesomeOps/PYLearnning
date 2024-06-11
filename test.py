b03892ccf8be47a69bc99f28954052dd8234dd0e
AwesomeOps <yuan.sn@outlook.com>
2024-06-11 20:56:08 +0800

    PYLEARNING

---
+++ test.py
@@ -0,0 +1,13 @@
+# print("Hello World!")
+# str1="今天吃什么"
+# print(nub_str1+nub_str2)
+# print("%s"  %str1)
+# user_name=input()
+# user_type=input()
+# print("你好：%s 您是尊贵的：%s" %(user_name,user_type))
+
+#布尔类型
+bool_1=1>2
+bool_2=2>1
+print(f"bool_1的类型为：{bool_1}")
+print(f"bool_1的类型为：{bool_2}")
