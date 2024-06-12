e6c1ec726b5f98be44dfda51a67c6d9f6d44e824
AwesomeOps <yuan.sn@outlook.com>
2024-06-12 17:04:21 +0800

    6.12

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-# print("Hello World!")
-# str1="今天吃什么"
-# print(nub_str1+nub_str2)
-# print("%s"  %str1)
-# user_name=input()
-# user_type=input()
-# print("你好：%s 您是尊贵的：%s" %(user_name,user_type))
+#打印
+str1="Hello World!"
+print(f"str1的内容为：{str1}")
+
+#输入
+user_name=input()
+user_type=input()
+print("你好：%s 您是尊贵的：%s" %(user_name,user_type))
 
 #布尔类型
 bool_1=1>2
 bool_2=2>1
-print(f"bool_1的类型为：{bool_1}")
-print(f"bool_1的类型为：{bool_2}")
-#123
-#123
-#123
+print(f"bool_1的类型为：{bool_1}"'\n'"bool_2的类型为：{bool_2}")
+
+
+
