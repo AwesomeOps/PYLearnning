d0f3e8b68d135e5e7d921a0e6b59ed729bf95b62
AwesomeOps <yuan.sn@outlook.com>
2024-06-13 00:00:32 +0800

    6/13

--- test.py
+++ test.py
@@ -1,16 +1,28 @@
-#打印
-str1="Hello World!"
-print(f"str1的内容为：{str1}")
+# #打印
+# str1="Hello World!"
+# print(f"str1的内容为：{str1}")
+#
+# #输入
+# user_name=input()
+# user_type=input()
+# print("你好：%s 您是尊贵的：%s" %(user_name,user_type))
+#
+# #布尔类型
+# bool_1=1>2
+# bool_2=2>1
+# print(f"bool_1的类型为：{bool_1}"'\n'"bool_2的类型为：{bool_2}")
+#
+# #多条件判断
+# num1=int(input("请输入一个数字: "))
+# if num1>0:
+#     print("输入大于0")
+# elif num1<0:
+#     print("输入小于0")
+# else:
+#     print("等于0")
 
-#输入
-user_name=input()
-user_type=input()
-print("你好：%s 您是尊贵的：%s" %(user_name,user_type))
-
-#布尔类型
-bool_1=1>2
-bool_2=2>1
-print(f"bool_1的类型为：{bool_1}"'\n'"bool_2的类型为：{bool_2}")
-
+#while嵌套
+i=1
+while i<
 
 
