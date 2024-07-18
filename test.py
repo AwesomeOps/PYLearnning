391c195f4e017dbaa9a0e0bfa7a7ff13a92f7fdc
AwesomeOps <Yuan.Sn@outlook.com>
2024-07-18 23:45:54 +0800

    7.18_2

--- test.py
+++ test.py
@@ -78,23 +78,53 @@ import math
 # print(f"{a:.2f}")
 
 #面向对象的编程
-class Student:
-    def __init__(self,name,student_id):
-        self.name=name
-        self.student_id=student_id
-        self.grades={"语文":0,"数学":0,"英语":0}
-    def set_grade(self,course,grade):
-        if course in self.grades:
-            self.grades[course]=grade
-    def print_grades(self):
-        print(f"学生{self.name}(学号:{self.student_id})的成绩为：")
-        for course in self.grades:
-            print(f"{course}:{self.grades[course]}分")
-chen=Student("小陈","1234")
-chen.set_grade("语文",92)
-chen.set_grade("数学",98)
-chen.set_grade("英语",95)
-chen.print_grades()
+# class Student:
+#     def __init__(self,name,student_id):
+#         self.name=name
+#         self.student_id=student_id
+#         self.grades={"语文":0,"数学":0,"英语":0}
+#     def set_grade(self,course,grade):
+#         if course in self.grades:
+#             self.grades[course]=grade
+#     def print_grades(self):
+#         print(f"学生{self.name}(学号:{self.student_id})的成绩为：")
+#         for course in self.grades:
+#             print(f"{course}:{self.grades[course]}分")
+# chen=Student("小陈","1234")
+# chen.set_grade("语文",92)
+# chen.set_grade("数学",98)
+# chen.set_grade("英语",95)
+# chen.print_grades()
+
+#继承
+# class Employee:
+#     def __init__(self,name,id):
+#         self.name=name
+#         self.id=id
+#     def print_info(self):
+#         print(f"员工名字:{self.name},工号:{self.id}")
+#
+# class FullTimeEmployee(Employee):
+#     def __init__(self,name,id,monthly_salary):
+#         super().__init__(name,id)
+#         self.monthly_salary=monthly_salary
+#     def calculate_monthly_pay(self):
+#         return self.monthly_salary
+# class PartTimeEmployee(Employee):
+#     def __init__(self,name,id,daily_salary,work_days):
+#         super().__init__(name,id)
+#         self.daily_salary=daily_salary
+#         self.work_days=work_days
+#     def calculate_monthly_pay(self):
+#         return self.daily_salary*self.work_days
+# zhangsan=FullTimeEmployee("张三","1001",6000)
+# lisi=PartTimeEmployee("李四","1002",230,15)
+# zhangsan.print_info()
+# lisi.print_info()
+# print(zhangsan.calculate_monthly_pay())
+# print((lisi.calculate_monthly_pay()))
+
+
 
 
 
